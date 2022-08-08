af74034ef6698a3a8525beb7ae97c33da82bd1b8
ANG <chikkoy99@gmail.com>
2022-08-07 23:09:20 -0300

    Pausa/PingPong
    hay otras cosas pero no me acuerdo

---
+++ Proyecto para-digma/Assets/Scenes/pausa.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pausa : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume ()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+    public void CloseGame()
+    {
+    Debug.Log("Saliendo");
+        Application.Quit();
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
